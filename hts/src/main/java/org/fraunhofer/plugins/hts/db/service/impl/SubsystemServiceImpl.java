c0df796af1e061e834c43205f65e7e43b0f6c7bc
llayman <llayman@fc-md.umd.edu>
2014-04-21 11:09:30 -0400

    - Added implementation and test for Subsystems and task #62

--- hts/src/main/java/org/fraunhofer/plugins/hts/db/service/impl/SubsystemServiceImpl.java
+++ hts/src/main/java/org/fraunhofer/plugins/hts/db/service/impl/SubsystemServiceImpl.java
@@ -1,5 +1,8 @@
 package org.fraunhofer.plugins.hts.db.service.impl;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.Lists.newArrayList;
+
 import java.util.List;
 
 import net.java.ao.Query;
@@ -9,12 +12,12 @@ import org.fraunhofer.plugins.hts.db.service.SubsystemService;
 
 import com.atlassian.activeobjects.external.ActiveObjects;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.collect.Lists.newArrayList;
-
 public class SubsystemServiceImpl implements SubsystemService {
 	private final ActiveObjects ao;
 
+	private static boolean initialized = false;
+	private static Object _lock = new Object();
+	
 	public SubsystemServiceImpl(ActiveObjects ao) {
 		this.ao = checkNotNull(ao);
 	}
@@ -25,17 +28,19 @@ public class SubsystemServiceImpl implements SubsystemService {
 		subsys.setLabel(label);
 		subsys.setDescription(subsysDesc);
 		subsys.save();
-		return null;
+		return subsys;
 	}
 
 	@Override
 	public Subsystems getSubsystemByID(String id) {
+		initializeTable();
 		final Subsystems[] subsystem = ao.find(Subsystems.class, Query.select().where("ID=?", id));
 		return subsystem.length > 0 ? subsystem[0] : null;
 	}
 
 	@Override
 	public List<Subsystems> all() {
+		initializeTable();
 		return newArrayList(ao.find(Subsystems.class));
 	}
 
@@ -67,6 +72,46 @@ public class SubsystemServiceImpl implements SubsystemService {
 		}
 		
 		return listAll;
+	}
+	
+	public void initializeTable() {
+		synchronized (_lock) {
+			if (!initialized) {
+				if (ao.find(Subsystems.class).length == 0) {
+					add("Structure","");
+					add("Power","Battery, Solar Arrays, Electrical Distribution");
+					add("Communications","Antennas, HGA");
+					add("Propulsion","");
+					add("Guidance and Control","Reaction Wheels");
+					add("Instrument","");
+					add("Ordnance/Pyro","");
+					add("Hazardous Materials","");
+					add("GSE Electrical Materials","");
+					add("GSE Mechanical Materials","");
+					add("System Interfaces","S/C, Human, Facility");
+					add("Environmental","");
+					add("Ionized Radiation","");
+					add("Non-Ionized Radiation","");
+					add("Propellants","");
+					add("Electrical/Electronics","");
+					add("Human Factors","");
+				}
+				initialized = true;
+			}
+		}
+
+	}
+
+	public static boolean isInitialized() {
+		synchronized (_lock) {
+			return initialized;
+		}
+	}
+
+	public static void reset() {
+		synchronized (_lock) {
+			initialized = false;
+		}
 	}
 	
 }
---
+++ hts/src/test/java/ut/org/fraunhofer/plugins/hts/db/service/impl/SubsystemServiceImplTest.java
@@ -0,0 +1,186 @@
+package ut.org.fraunhofer.plugins.hts.db.service.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyVararg;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import net.java.ao.DBParam;
+import net.java.ao.Query;
+
+import org.fraunhofer.plugins.hts.db.Subsystems;
+import org.fraunhofer.plugins.hts.db.service.impl.SubsystemServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.atlassian.activeobjects.external.ActiveObjects;
+
+public class SubsystemServiceImplTest {
+
+	Subsystems mockSubsystem;
+	Subsystems[] mockSubsystems;
+	ActiveObjects mockActiveObjects;
+
+	@Before
+	public void setUp() {
+		SubsystemServiceImpl.reset();
+
+		mockSubsystem = mock(Subsystems.class);
+		mockSubsystems = new Subsystems[] { mockSubsystem };
+		mockActiveObjects = mock(ActiveObjects.class);
+
+		when(mockActiveObjects.create(eq(Subsystems.class), (DBParam[]) anyVararg())).thenReturn(mockSubsystem);
+		when(mockActiveObjects.find(eq(Subsystems.class))).thenReturn(mockSubsystems);
+	}
+
+	@Test
+	public void testAdd() {
+		SubsystemServiceImpl test = new SubsystemServiceImpl(mockActiveObjects);
+		Subsystems result = test.add("blah", "blah");
+
+		assertEquals(mockSubsystem, result);
+
+		verify(mockSubsystem).setLabel("blah");
+		verify(mockSubsystem).setDescription("blah");
+		verify(mockSubsystem).save();
+	}
+
+	@Test
+	public void testGetHazardGroupByID() {
+		Subsystems mockSubsystem2 = mock(Subsystems.class);
+		Subsystems[] mockSubsystems = { mockSubsystem, mockSubsystem2 };
+
+		when(mockActiveObjects.find(eq(Subsystems.class), any(Query.class))).thenReturn(mockSubsystems);
+
+		SubsystemServiceImpl test = new SubsystemServiceImpl(mockActiveObjects);
+		Subsystems result = test.getSubsystemByID("blah");
+
+		assertTrue(SubsystemServiceImpl.isInitialized());
+		verify(mockActiveObjects).find(eq(Subsystems.class), any(Query.class));
+		assertEquals(result, mockSubsystem);
+
+	}
+
+	@Test
+	public void testGetHazardGroupByIDNoneFound() {
+		Subsystems[] mockSubsystems = {};
+
+		when(mockActiveObjects.find(eq(Subsystems.class), any(Query.class))).thenReturn(mockSubsystems);
+
+		SubsystemServiceImpl test = new SubsystemServiceImpl(mockActiveObjects);
+		Subsystems result = test.getSubsystemByID("blah");
+
+		assertTrue(SubsystemServiceImpl.isInitialized());
+		verify(mockActiveObjects).find(eq(Subsystems.class), any(Query.class));
+		assertNull(result);
+
+	}
+
+	@Test
+	public void testAllNoneFound() {
+		Subsystems[] mockSubsystems = {};
+
+		when(mockActiveObjects.find(eq(Subsystems.class))).thenReturn(mockSubsystems);
+
+		SubsystemServiceImpl test = new SubsystemServiceImpl(mockActiveObjects);
+		List<Subsystems> result = test.all();
+
+		assertTrue(SubsystemServiceImpl.isInitialized());
+		assertTrue(result.isEmpty());
+	}
+
+	@Test
+	public void testAllMultipleFound() {
+		Subsystems[] mockSubsystems = { mockSubsystem, mockSubsystem, mockSubsystem };
+
+		when(mockActiveObjects.find(eq(Subsystems.class))).thenReturn(mockSubsystems);
+
+		SubsystemServiceImpl test = new SubsystemServiceImpl(mockActiveObjects);
+		List<Subsystems> result = test.all();
+
+		assertTrue(SubsystemServiceImpl.isInitialized());
+		assertEquals(result.size(), 3);
+	}
+
+	@Test
+	public void testInitializeTables() {
+		Subsystems[] mockSubsystems = {};
+		when(mockActiveObjects.find(eq(Subsystems.class))).thenReturn(mockSubsystems);
+
+		SubsystemServiceImpl test = new SubsystemServiceImpl(mockActiveObjects);
+		test.initializeTable();
+
+		verify(mockActiveObjects, times(17)).create(eq(Subsystems.class), (DBParam[]) anyVararg());
+
+		verify(mockSubsystem).setLabel("Structure");
+		verify(mockSubsystem).setLabel("Power");
+		verify(mockSubsystem).setLabel("Communications");
+		verify(mockSubsystem).setLabel("Propulsion");
+		verify(mockSubsystem).setLabel("Guidance and Control");
+		verify(mockSubsystem).setLabel("Instrument");
+		verify(mockSubsystem).setLabel("Ordnance/Pyro");
+		verify(mockSubsystem).setLabel("Hazardous Materials");
+		verify(mockSubsystem).setLabel("GSE Electrical Materials");
+		verify(mockSubsystem).setLabel("GSE Mechanical Materials");
+		verify(mockSubsystem).setLabel("System Interfaces");
+		verify(mockSubsystem).setLabel("Environmental");
+		verify(mockSubsystem).setLabel("Ionized Radiation");
+		verify(mockSubsystem).setLabel("Non-Ionized Radiation");
+		verify(mockSubsystem).setLabel("Propellants");
+		verify(mockSubsystem).setLabel("Electrical/Electronics");
+		verify(mockSubsystem).setLabel("Human Factors");
+	}
+
+	@Test
+	public void testInitializeTablesAlreadyInitialized() {
+		Subsystems[] mockSubsystems = {};
+		when(mockActiveObjects.find(eq(Subsystems.class))).thenReturn(mockSubsystems);
+
+		SubsystemServiceImpl test = new SubsystemServiceImpl(mockActiveObjects);
+		test.initializeTable();
+		test.initializeTable();
+		
+		verify(mockActiveObjects, times(17)).create(eq(Subsystems.class), (DBParam[]) anyVararg());
+
+		verify(mockSubsystem).setLabel("Structure");
+		verify(mockSubsystem).setLabel("Power");
+		verify(mockSubsystem).setLabel("Communications");
+		verify(mockSubsystem).setLabel("Propulsion");
+		verify(mockSubsystem).setLabel("Guidance and Control");
+		verify(mockSubsystem).setLabel("Instrument");
+		verify(mockSubsystem).setLabel("Ordnance/Pyro");
+		verify(mockSubsystem).setLabel("Hazardous Materials");
+		verify(mockSubsystem).setLabel("GSE Electrical Materials");
+		verify(mockSubsystem).setLabel("GSE Mechanical Materials");
+		verify(mockSubsystem).setLabel("System Interfaces");
+		verify(mockSubsystem).setLabel("Environmental");
+		verify(mockSubsystem).setLabel("Ionized Radiation");
+		verify(mockSubsystem).setLabel("Non-Ionized Radiation");
+		verify(mockSubsystem).setLabel("Propellants");
+		verify(mockSubsystem).setLabel("Electrical/Electronics");
+		verify(mockSubsystem).setLabel("Human Factors");	}
+
+	@Test
+	public void testIsInitialized() {
+		assertFalse(SubsystemServiceImpl.isInitialized());
+	}
+
+	@Test
+	public void testReset() {
+		SubsystemServiceImpl test = new SubsystemServiceImpl(mockActiveObjects);
+		test.initializeTable();
+
+		assertTrue(SubsystemServiceImpl.isInitialized());
+		SubsystemServiceImpl.reset();
+		assertFalse(SubsystemServiceImpl.isInitialized());
+	}
+}
