bb1f63bca3bcf4a1272efebb715ee84c0aa4c3e7
Arnar Pall Skulason <arnars11@ru.is>
2014-05-14 14:20:24 -0400

    Clicking the cause title to open is now possible

--- hts/src/main/resources/js/htsCausePage.js
+++ hts/src/main/resources/js/htsCausePage.js
@@ -1,16 +1,15 @@
 AJS.$(document).ready(function(){
 	AJS.$('.rowGroup .formContainer').hide();
 
-	AJS.$('.cell span').click(function() {
-		console.log("HEHE");
-		var spanClass = AJS.$(this).attr("class");
+	AJS.$('.toggle').click(function() {
+		var spanElement = AJS.$(this).children();
+		var spanClass = spanElement.attr("class");
 		var formCont = AJS.$(this).parent().parent().parent().find('.formContainer');
-		console.log(formCont.is(":visible"));
 		if(!(formCont.is(":visible"))) {
-			AJS.$(this).removeClass(spanClass).addClass("aui-icon aui-icon-small aui-iconfont-devtools-task-disabled");
+			spanElement.removeClass(spanClass).addClass("aui-icon aui-icon-small aui-iconfont-devtools-task-disabled");
 		}
 		else {
-			AJS.$(this).removeClass(spanClass).addClass("aui-icon aui-icon-small aui-iconfont-add");
+			spanElement.removeClass(spanClass).addClass("aui-icon aui-icon-small aui-iconfont-add");
 		}
 	    formCont.slideToggle();
 	});
