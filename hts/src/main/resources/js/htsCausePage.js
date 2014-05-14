ec7acb4d043dc31d8624b5bac5859dd31e4ef883
Arnar Pall Skulason <arnars11@ru.is>
2014-05-14 13:48:03 -0400

    List is now working, plus sign opens the list

--- hts/src/main/resources/js/htsCausePage.js
+++ hts/src/main/resources/js/htsCausePage.js
@@ -1,7 +1,17 @@
 AJS.$(document).ready(function(){
 	AJS.$('.rowGroup .formContainer').hide();
 
-	AJS.$('.rowGroup').click(function() {
-	    AJS.$(this).find('.formContainer').slideToggle();
+	AJS.$('.cell span').click(function() {
+		console.log("HEHE");
+		var spanClass = AJS.$(this).attr("class");
+		var formCont = AJS.$(this).parent().parent().parent().find('.formContainer');
+		console.log(formCont.is(":visible"));
+		if(!(formCont.is(":visible"))) {
+			AJS.$(this).removeClass(spanClass).addClass("aui-icon aui-icon-small aui-iconfont-devtools-task-disabled");
+		}
+		else {
+			AJS.$(this).removeClass(spanClass).addClass("aui-icon aui-icon-small aui-iconfont-add");
+		}
+	    formCont.slideToggle();
 	});
 });
