94450b627ccc2545c45531cc8c2e59da885e8d9d
Arnar Pall Skulason <arnars11@ru.is>
2014-05-15 16:49:57 -0400

    Expanding and closing works, need to change the signs on cause #

--- hts/src/main/resources/js/htsCausePage.js
+++ hts/src/main/resources/js/htsCausePage.js
@@ -1,11 +1,28 @@
+function checkElementExpansion(element) {
+	return element.is(":visible");
+}
+
 AJS.$(document).ready(function(){
+	AJS.$("#expandAll").live('click', function() {
+		console.log(AJS.$(this));
+		AJS.$(".rowGroup .formContainer").slideToggle();
+		console.log(AJS.$(".formContainer").is(":visible"));
+		console.log(AJS.$(this).html());
+		if(AJS.$(this).html() === "Close all") {
+			AJS.$(this).html("Expand all");	
+		}
+		else {
+			AJS.$(this).html("Close all");
+		}
+	});
+
 	AJS.$('.rowGroup .formContainer').hide();
 
 	AJS.$('.toggle').click(function() {
 		var spanElement = AJS.$(this).children();
 		var spanClass = spanElement.attr("class");
 		var formCont = AJS.$(this).parent().parent().find('.formContainer');
-		if(!(formCont.is(":visible"))) {
+		if(!(checkElementExpansion(formCont))) {
 			spanElement.removeClass(spanClass).addClass("aui-icon aui-icon-small aui-iconfont-devtools-task-disabled");
 		}
 		else {
