72db281492902be4a86fc3d944f5ad63eccff5bc
Brynjar Bragason <brysbra@gmail.com>
2014-08-04 15:37:20 -0400

    Linking up the Report Generator itself to the Report Generator servlet.

--- hts/src/main/java/org/fraunhofer/plugins/hts/servlet/ReportGenerationServlet.java
+++ hts/src/main/java/org/fraunhofer/plugins/hts/servlet/ReportGenerationServlet.java
@@ -4,6 +4,8 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -11,11 +13,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.fraunhofer.plugins.hts.db.Hazards;
+import org.fraunhofer.plugins.hts.db.Review_Phases;
+import org.fraunhofer.plugins.hts.db.Risk_Categories;
+import org.fraunhofer.plugins.hts.db.Risk_Likelihoods;
+import org.fraunhofer.plugins.hts.db.service.HazardService;
+import org.fraunhofer.plugins.hts.db.service.ReviewPhaseService;
+import org.fraunhofer.plugins.hts.db.service.RiskCategoryService;
+import org.fraunhofer.plugins.hts.db.service.RiskLikelihoodsService;
+import org.fraunhofer.plugins.hts.document.HazardReportGenerator;
+
+import com.google.common.collect.Lists;
+
 public final class ReportGenerationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final HazardReportGenerator hazardReportGenerator;
+	private final HazardService hazardService;
+	private final ReviewPhaseService reviewPhaseService;
+	private final RiskCategoryService riskCategoryService;
+	private final RiskLikelihoodsService riskLikelihoodsService;
 
+	public ReportGenerationServlet(HazardReportGenerator hazardReportGenerator, HazardService hazardService, 
+			ReviewPhaseService reviewPhaseService, RiskCategoryService riskCategoryService, 
+			RiskLikelihoodsService riskLikelihoodsService) {
+		this.hazardReportGenerator = hazardReportGenerator;
+		this.hazardService = hazardService;
+		this.reviewPhaseService = reviewPhaseService;
+		this.riskCategoryService = riskCategoryService;
+		this.riskLikelihoodsService = riskLikelihoodsService;
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		final String hazardID = request.getParameter("hazardToDownload");
+		List<File> hazardFiles = setupWordDocuments(hazardID);
+		
+		
 		//get the 'file' parameter
 		
 	    //String fileName = (String) request.getParameter("file");
@@ -62,5 +96,17 @@ public final class ReportGenerationServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+	}
+	
+	private List<File> setupWordDocuments(String hazardID) {
+		Hazards currentHazard = hazardService.getHazardByID(hazardID);
+		List<Hazards> hazardList = Lists.newArrayList(currentHazard);
+		
+		List<Review_Phases> reviewPhasesList = new ArrayList<Review_Phases>(reviewPhaseService.all());
+		List<Risk_Categories> riskCategoriesList = new ArrayList<Risk_Categories>(riskCategoryService.all());
+		List<Risk_Likelihoods> riskLikelihoodsList = new ArrayList<Risk_Likelihoods>(riskLikelihoodsService.all());
+		// one more parameter left to initialize before function call
+		
+		return null;
 	}
 }
