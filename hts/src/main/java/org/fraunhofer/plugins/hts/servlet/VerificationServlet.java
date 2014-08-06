7c555442c88c4c9f9e8a3bed66bd1bb72da8f7d4
Brynjar Bragason <brysbra@gmail.com>
2014-08-06 10:40:14 -0400

    JIRA error was coming up, Verification page not found. Fixed that.

--- hts/src/main/java/org/fraunhofer/plugins/hts/servlet/VerificationServlet.java
+++ hts/src/main/java/org/fraunhofer/plugins/hts/servlet/VerificationServlet.java
@@ -3,20 +3,35 @@ package org.fraunhofer.plugins.hts.servlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.templaterenderer.TemplateRenderer;
+import com.google.common.collect.Maps;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+import java.util.Map;
 
 public class VerificationServlet extends HttpServlet{
-    private static final Logger log = LoggerFactory.getLogger(VerificationServlet.class);
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = LoggerFactory.getLogger(VerificationServlet.class);
+	private final TemplateRenderer templateRenderer;
+	
+	public VerificationServlet(TemplateRenderer templateRenderer) {
+		this.templateRenderer = templateRenderer;
+	}
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-    {
-        resp.setContentType("text/html");
-        resp.getWriter().write("<html><body>Hello World</body></html>");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	if (ComponentAccessor.getJiraAuthenticationContext().isLoggedInUser()) {
+    		Map<String, Object> context = Maps.newHashMap();
+    		resp.setContentType("text/html;charset=utf-8");
+    		context.put("baseUrl", ComponentAccessor.getApplicationProperties().getString("jira.baseurl"));
+    		templateRenderer.render("templates/EditHazard.vm", context, resp.getWriter());
+    	}
     }
 
 }
