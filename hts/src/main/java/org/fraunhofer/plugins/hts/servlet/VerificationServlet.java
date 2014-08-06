caf464cc7b89b1598587314b3fc520ce2f623bc0
Brynjar Bragason <brysbra@gmail.com>
2014-08-06 11:01:48 -0400

    doGet and doPost functions set up with minimal code.

--- hts/src/main/java/org/fraunhofer/plugins/hts/servlet/VerificationServlet.java
+++ hts/src/main/java/org/fraunhofer/plugins/hts/servlet/VerificationServlet.java
@@ -1,7 +1,11 @@
 package org.fraunhofer.plugins.hts.servlet;
 
+import org.fraunhofer.plugins.hts.db.Hazards;
+import org.fraunhofer.plugins.hts.db.service.HazardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.templaterenderer.TemplateRenderer;
@@ -19,9 +23,11 @@ public class VerificationServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	private static final Logger log = LoggerFactory.getLogger(VerificationServlet.class);
 	private final TemplateRenderer templateRenderer;
+	private final HazardService hazardService;
 	
-	public VerificationServlet(TemplateRenderer templateRenderer) {
+	public VerificationServlet(TemplateRenderer templateRenderer, HazardService hazardService) {
 		this.templateRenderer = templateRenderer;
+		this.hazardService = checkNotNull(hazardService);
 	}
 
     @Override
@@ -30,7 +36,31 @@ public class VerificationServlet extends HttpServlet{
     		Map<String, Object> context = Maps.newHashMap();
     		resp.setContentType("text/html;charset=utf-8");
     		context.put("baseUrl", ComponentAccessor.getApplicationProperties().getString("jira.baseurl"));
-    		templateRenderer.render("templates/EditHazard.vm", context, resp.getWriter());
+    		if ("y".equals(req.getParameter("edit"))) {
+    			Hazards currentHazard = hazardService.getHazardByID(req.getParameter("key"));
+				context.put("hazardNumber", currentHazard.getHazardNum());
+				context.put("hazardTitle", currentHazard.getTitle());
+				context.put("hazardID", currentHazard.getID());
+				context.put("hazard", currentHazard);
+				templateRenderer.render("templates/EditHazard.vm", context, resp.getWriter());
+    		}
+    		else {
+    			templateRenderer.render("templates/HazardPage.vm", context, resp.getWriter());
+    		}
+    	}
+    	else {
+    		resp.sendRedirect(req.getContextPath() + "/login.jsp");
+    	}
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    	if ("y".equals(req.getParameter("edit"))) {
+    		res.sendRedirect(req.getContextPath() + "/plugins/servlet/verificationform");
+    	}
+    	else {
+    		final Hazards currentHazard = hazardService.getHazardByID(req.getParameter("hazardID"));
+    		res.sendRedirect(req.getContextPath() + "/plugins/servlet/causeform?edit=y&key=" + currentHazard.getID());
     	}
     }
 
