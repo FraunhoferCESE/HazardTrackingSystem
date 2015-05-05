6a4ad655efe351e67f15d84327dd13c28ff59a2f
Lucas Layman <llayman@fc-md.umd.edu>
2015-05-05 10:41:39 -0400

    Updated test case with transfer data for causes

--- hts/src/test/java/ut/org/fraunhofer/plugins/hts/document/HazardReportGeneratorTest.java
+++ hts/src/test/java/ut/org/fraunhofer/plugins/hts/document/HazardReportGeneratorTest.java
@@ -49,11 +49,11 @@ public class HazardReportGeneratorTest {
 		Risk_Likelihoods mockRiskLikelihoods = mock(Risk_Likelihoods.class);
 		when(mockRiskLikelihoods.getValue()).thenReturn("C - Occassional");
 		when(mockRiskLikelihoods.getID()).thenReturn(33333);
-		
+
 		Risk_Categories mockRiskCategories = mock(Risk_Categories.class);
 		when(mockRiskCategories.getValue()).thenReturn("I - Catastrophic");
 		when(mockRiskCategories.getID()).thenReturn(11111);
-		
+
 		mockCause1 = mock(Hazard_Causes.class);
 		when(mockCause1.getCauseNumber()).thenReturn(1);
 		when(mockCause1.getTitle()).thenReturn(
@@ -65,7 +65,6 @@ public class HazardReportGeneratorTest {
 		when(mockCause1.getAdditionalSafetyFeatures()).thenReturn("We will do extreme inspections on the feed.");
 		when(mockCause1.getRiskCategory()).thenReturn(mockRiskCategories);
 		when(mockCause1.getRiskLikelihood()).thenReturn(mockRiskLikelihoods);
-		
 
 		mockCause2 = mock(Hazard_Causes.class);
 		when(mockCause2.getCauseNumber()).thenReturn(2);
@@ -77,8 +76,7 @@ public class HazardReportGeneratorTest {
 		when(mockCause2.getAdditionalSafetyFeatures()).thenReturn("None.");
 		when(mockCause2.getRiskCategory()).thenReturn(mockRiskCategories);
 		when(mockCause2.getRiskLikelihood()).thenReturn(mockRiskLikelihoods);
-		
-		
+
 		mockCause3 = mock(Hazard_Causes.class);
 		when(mockCause3.getCauseNumber()).thenReturn(3);
 		when(mockCause3.getTitle()).thenReturn("Loss of fuel and/or oxidizer supply");
@@ -86,36 +84,31 @@ public class HazardReportGeneratorTest {
 				.thenReturn(
 						"Total loss of fuel and/or oxidizer supply would be caused by a failed close prevalve during USE operation. This could be from failure of the prevalve or of the pneumatic system/command to the prevalve. A single dual coil pneumatic valve controls the pressure to both prevalves. If this pneumatic valve (HF/OF -1000) is inadvertently activated the prevalves will close.\n\nShould there be a total loss of the fuel or oxidizer supply from the Upper Stage to the J-2X during operation, this would unload the Fuel or Oxidizer Turbopump and the pump could experience rotor overspeed which may induce pump or turbine component failure (including component rotational burst), and possibly FTP/OTP structural disintegration. This structural disintegration will cause turbopump loss of function, generation of shrapnel, uncontained USE damage and possibly fire/explosion, leading to loss of mission, and/or a loss of crew/vehicle. In addition loss of fuel supply only would cause the USE gas generator to run LO2 rich.");
 		when(mockCause3.getEffects()).thenReturn("end of the world.");
-		when(mockCause3.getAdditionalSafetyFeatures()).thenReturn("Analysis, simulation, and demonstration to 5 9's reliability, which doesn't make sense but whatever.");
+		when(mockCause3.getAdditionalSafetyFeatures()).thenReturn(
+				"Analysis, simulation, and demonstration to 5 9's reliability, which doesn't make sense but whatever.");
 		when(mockCause3.getRiskCategory()).thenReturn(mockRiskCategories);
 		when(mockCause3.getRiskLikelihood()).thenReturn(mockRiskLikelihoods);
 
-		
-		
 		mockCauseToHazard = mock(Hazard_Causes.class);
 		when(mockCauseToHazard.getCauseNumber()).thenReturn(4);
-		when(mockCauseToHazard.getTitle()).thenReturn("Fuel supply pressure too low or too high");
-		when(mockCauseToHazard.getDescription())
-				.thenReturn(
-						"A fuel supply pressure from vehicle that is too low could result in too-low of an FTP inducer inlet pressure leading to excessive cavitation, potentially unloading the pump and progressing to rotor overspeed, which may induce pump or turbine component failure (including component rotational burst). This could result in FTP structural disintegration.\nA fuel supply pressure from vehicle that is too high could result in increased inducer inlet pressure, altering axial load on pump rotary components, but the balance piston will compensate for the alteration, and net axial thrust of the FTP will remain unchanged at zero.\nFTP structural disintegration will cause turbopump loss of function and possibly the generation of shrapnel. The cumulative effect is uncontained USE damage, and possiblyfire, leading to loss of mission, and/or a loss of crew/vehicle. (from USE-E-01 cause O)");
+		// when(mockCauseToHazard.getTitle()).thenReturn("Fuel supply pressure too low or too high");
+		when(mockCauseToHazard.getDescription()).thenReturn("TRansferred to a relevant Hazard here");
 		when(mockCauseToHazard.getTransfer()).thenReturn(12345);
-		when(mockCauseToHazard.getEffects()).thenReturn("Itching and burning.");
-		when(mockCauseToHazard.getAdditionalSafetyFeatures()).thenReturn("gold Bond Medical powder helps.");
-		when(mockCauseToHazard.getRiskCategory()).thenReturn(mockRiskCategories);
-		when(mockCauseToHazard.getRiskLikelihood()).thenReturn(mockRiskLikelihoods);
+		// when(mockCauseToHazard.getEffects()).thenReturn("Itching and burning.");
+		// when(mockCauseToHazard.getAdditionalSafetyFeatures()).thenReturn("gold Bond Medical powder helps.");
+		// when(mockCauseToHazard.getRiskCategory()).thenReturn(mockRiskCategories);
+		// when(mockCauseToHazard.getRiskLikelihood()).thenReturn(mockRiskLikelihoods);
 
-		
 		mockCauseToCause = mock(Hazard_Causes.class);
 		when(mockCauseToCause.getCauseNumber()).thenReturn(5);
-		when(mockCauseToCause.getTitle()).thenReturn("Excessive POGO creates surges");
-		when(mockCauseToCause.getDescription())
-				.thenReturn(
-						"POGO loads would create excessive stress and pressure/ flow fluctuations that could lead to cavitation");
+		// when(mockCauseToCause.getTitle()).thenReturn("Excessive POGO creates surges");
+		when(mockCauseToCause.getDescription()).thenReturn(
+				"This cause is described in another hazard cause. Look it up.");
 		when(mockCauseToCause.getTransfer()).thenReturn(9876);
-		when(mockCauseToCause.getEffects()).thenReturn("Catastrohpic structure failure.");
-		when(mockCauseToCause.getAdditionalSafetyFeatures()).thenReturn("MECO when POGO is detected.");
-		when(mockCauseToCause.getRiskCategory()).thenReturn(mockRiskCategories);
-		when(mockCauseToCause.getRiskLikelihood()).thenReturn(mockRiskLikelihoods);
+		// when(mockCauseToCause.getEffects()).thenReturn("Catastrohpic structure failure.");
+		// when(mockCauseToCause.getAdditionalSafetyFeatures()).thenReturn("MECO when POGO is detected.");
+		// when(mockCauseToCause.getRiskCategory()).thenReturn(mockRiskCategories);
+		// when(mockCauseToCause.getRiskLikelihood()).thenReturn(mockRiskLikelihoods);
 
 	}
 
@@ -172,7 +165,6 @@ public class HazardReportGeneratorTest {
 		when(mockHazardCauseService.getHazardCauseByID(Integer.toString(transferDestinationCause.getID()))).thenReturn(
 				transferDestinationCause);
 
-		
 		Project mockProject1 = mock(Project.class);
 		when(mockProject1.getName()).thenReturn("Project named AAAAA");
 		Project mockProject2 = mock(Project.class);
@@ -300,7 +292,8 @@ public class HazardReportGeneratorTest {
 				new Hazard_Causes[] { mockCause1, mockCause2, mockCause3, mockCauseToHazard, mockCauseToCause });
 
 		List<Hazards> hazardList = Lists.newArrayList(testHazard);
-		HazardReportGenerator test = new HazardReportGenerator(mockHazardService, mockHazardCauseService, mockTransferService, mockProjectManager);
+		HazardReportGenerator test = new HazardReportGenerator(mockHazardService, mockHazardCauseService,
+				mockTransferService, mockProjectManager);
 
 		final File template = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "src"
 				+ System.getProperty("file.separator") + "main" + System.getProperty("file.separator") + "resources"
