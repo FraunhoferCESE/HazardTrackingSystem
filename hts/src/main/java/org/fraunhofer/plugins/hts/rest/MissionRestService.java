7729bd0b6e233ecf19c90034f9d996b72159ff75
Lucas Layman <llayman@fc-md.umd.edu>
2015-05-09 18:22:27 -0400

    Completed dynamic hazard navigation

--- hts/src/main/java/org/fraunhofer/plugins/hts/rest/MissionRestService.java
+++ hts/src/main/java/org/fraunhofer/plugins/hts/rest/MissionRestService.java
@@ -56,4 +56,6 @@ public class MissionRestService {
 					.build();
 		}
 	}
+	
+	
 }
--- hts/src/main/resources/js/hazard-navigation.js
+++ hts/src/main/resources/js/hazard-navigation.js
@@ -2,31 +2,54 @@
  * 
  */
 
-
-
 function initializeHazardNavigation() {
-
 	projectSelect = AJS.$("#missionNavigation");
 	projectSelect.children().remove();
-	
+
 	hazardSelect = AJS.$("#hazardNavigation");
 	hazardSelect.children().remove();
 
 	var projects = getProjectsForUser();
 	if (projects.length == 0) {
-		projectSelect.append("<option></option>").text("<No Projects Available>");
+		projectSelect.append("<option></option>").text(
+				"<No Projects Available>").attr("value", -1);
 	} else {
 		AJS.$.each(projects, function(index, value) {
 			projectSelect.append(AJS.$("<option></option>").attr("value",
 					value.id).text(value.name));
 		});
-		
+
+		projectSelect.val(AJS.$("hazard").attr("projectId"))
 		projectSelect.change(function() {
-			alert("I changed");
+			hazardSelect.children().remove();
+			selectProject(projectSelect.val());
 		});
+		
+		selectProject(AJS.$("hazard").attr("projectId"));
+		hazardSelect.val(AJS.$("hazard").attr("currentId"));
+		
+		AJS.$("#gotoSelectedHazardReport").click(function() {
+			AJS.$("form#hazardNavForm input#selectedHazard").val(hazardSelect.val());
+		});
+		
 	}
 }
 
+function selectProject(projectId) {
+	if (typeof projectId !== "undefined" && projectId != -1) {
+		var hazards = getHazardsForProject(projectId);
+		if (hazards.length == 0) {
+			hazardSelect.append("<option></option>")
+				.text("<No Hazards for Project>");
+		} else {
+			AJS.$.each(hazards, function(index, value) {
+				hazardSelect.append(AJS.$("<option></option>")
+						.attr("value",value.hazardID)
+						.text(value.hazardNumber == null ? "<Hazard id=" + value.hazardID + ">" : value.hazardNumber));
+			});
+		}
+	}
+}
 
 function getProjectsForUser() {
 	var projects = [];
@@ -45,20 +68,21 @@ function getProjectsForUser() {
 	return projects;
 }
 
-function getHazardsForProject() {
-	var projects = [];
-	AJS.$.ajax({
-		type : "GET",
-		url : AJS.params.baseURL + "/rest/hts/1.0/mission/user",
-		async : false,
-		success : function(data) {
-			projects = data;
-		},
-		error : function() {
-			console.log("ERROR");
-		}
-	});
+function getHazardsForProject(projectId) {
+	var hazards = [];
+	AJS.$
+			.ajax({
+				type : "GET",
+				url : AJS.params.baseURL + "/rest/hts/1.0/mission/hazards/"
+						+ projectId,
+				async : false,
+				success : function(data) {
+					hazards = data;
+				},
+				error : function() {
+					console.log("ERROR");
+				}
+			});
 
-	return projects;
+	return hazards;
 }
-
